0c57d102cba6b054154e8670bbaf6be1e2fb469a
Jose Castro <drakonc@users.noreply.github.com>
2019-05-22 11:59:48 -0500

    Iniciando Script base de datos

--- src/app/controller/ticket.controller.ts
+++ src/app/controller/ticket.controller.ts
@@ -2,8 +2,10 @@ import { Request, Response } from 'express';
 
 class TicketController {
 
-    public addTicket(req: Request, res: Response){
-        res.render('ticket/addticket')
+    public addTicket(req: Request, res: Response) {
+        let list: any[] = [{ id: 1 }, { id: 2 }, { id: 3 }];
+        let p: any[] = [{ nom: 'hola' }, { nom: 'mundo' }]
+        res.render('ticket/addticket', { list, p })
     }
 }
 
